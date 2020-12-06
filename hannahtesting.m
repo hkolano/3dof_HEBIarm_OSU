0e2fd90e24f2021dd6d6a485aa96082ac91d9387
Aiden Shaevitz <39567668+Shaevitzaiden@users.noreply.github.com>
2020-12-06 15:34:17 -0800

    Update hannahtesting.m

--- hannahtesting.m
+++ hannahtesting.m
@@ -2,30 +2,36 @@
 startup()
 
 % % Set up the arm as a group
-% family = '3dof';
-% names = {'Base1', 'Base2', 'Elbow'};
-% group = HebiLookup.newGroupFromNames(family, names);
+family = '3dof';
+names = {'Base1', 'Base2', 'Elbow'};
+group = HebiLookup.newGroupFromNames(family, names);
 
 % If not connected to any physical modules
-group = HebiUtils.newImitationGroup(3)
+%group = HebiUtils.newImitationGroup(3)
 
 %% ---------- KINEMATICS ----------
 kin = HebiKinematics('3dofDescription');
 
+%% ---------- FEEDBACK ----------
+fbk = group.getNextFeedback;
 
 %% ---------- MOVEMENT ----------
 numModules = group.getNumModules;
 
 cmd = CommandStruct();
-cmd.position = zeros(1,numModules);
+% cmd.position = zeros(1,numModules);
 cmd.velocity = zeros(1,numModules);
 cmd.effort = [];
 tic;
 
 % send position/velocity command for 10 seconds
-while toc < 10
+while toc < 3
+  % cmd.position = [0, 1.5, 0];
+  cmd.velocity = [0.5, -0.5, 0];
   fbk = group.getNextFeedback();  % Use getNextFeedback() to                               % limit loop rate.
   group.send(cmd);
+  disp(fbk)
+  toc
 end
 
 
