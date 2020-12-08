dda480479569d91dd5975e58584271fc9a48d606
Aiden Shaevitz <39567668+Shaevitzaiden@users.noreply.github.com>
2020-12-07 22:43:01 -0800

    Please Check Me

--- hannahtesting.m
+++ hannahtesting.m
@@ -26,17 +26,21 @@ cmd.effort = [];
 tic;
 
 %% ------ Main ------
-targets = [0.3, 0.3, 0.3;
-           -0.2, -0.5, 0.2;
-           0.5, 0.1, 0.1];
+% targets = [0.09, 0.0, -0.04;
+%            0.09, -0.17, -0.04;
+%            0.09, 0.17, -0.04;
+%            0.09, 0, -0.04];
+targets = [0, 0, 1;
+           .2, .2, 0];
 positions = getWaypoints(targets, fbk, kin);
 disp(positions)
 moveArm(positions, group, cmd, trajGen)
 
-% % send position/velocity command for 10 seconds
-% while toc < 1
-%   % cmd.position = [0, 1.5, 0];
-%   cmd.velocity = [0, 0.5, 0];
+% send position/velocity command for 1 seconds
+% while toc < 2
+%   cmd.position = [0, 1.5, 0];
+%   % cmd.velocity = [-0.4, -1, 0];
+%   % cmd.velocity = [0.1, 0, 0];
 %   % kin.getForwardKinematics
 %   fbk = group.getNextFeedback();  % Use getNextFeedback() to                               % limit loop rate.
 %   % disp(kin.getForwardKinematics('endeffector', fbk.position));
@@ -49,27 +53,27 @@ moveArm(positions, group, cmd, trajGen)
 %% ---------- Helper Functions ----------
 function positions = getWaypoints(targets, fbk, kin)
     [rows, cols] = size(targets);
-    positions = zeros(rows, cols);
-    initialJointAngs = fbk.position;
-    for i = 1:rows
+    positions = zeros(rows+1, cols);
+    positions(1,:) = fbk.position;
+    disp(positions)
+    for i = 2:rows
         positions(i,:) = kin.getInverseKinematics( 'XYZ', targets(i,:),...
-            'InitialPositions', initialJointAngs );
-        initialJointAngs = positions(i,:);
+            'InitialPositions', positions(i,:));
     end
+    disp(positions)
 end
 
 
 function [] = moveArm(positions, group, cmd, trajGen)
-[rows, cols] = size(positions);
-timeToMove = 6;
+[rows, ~] = size(positions);
+timeToMove = 20;
 time = [0 timeToMove];
-trajGen.setSpeedFactor(0.6);
+% trajGen.setSpeedFactor(1);
 for i = 2:rows
     start = positions(i-1, :);
     finish = positions(i, :);
     trajectory = trajGen.newJointMove(...
-        [start; finish], ...
-        'time', time);
+        [start; finish],'time', time);
     
     fbk = group.getNextFeedback();
     t0 = fbk.time;
